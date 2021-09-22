2da90b3d219b7afa6cf05b0b3ba12116301741a9
darren.sargent <darren.sargent@cognizant.ai>
2021-09-22 10:32:14 -0700

    Use fancy staged build Dockerfile. Use proper tags for versions.
    Also error handling for streaming data.

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Index(): React.ReactElement {
         <meta name="description" content="Evolutionary AI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Under Pressure
+      LEAF v0.0.1
     </div>
   )
 }
