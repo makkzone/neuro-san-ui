88ea9d8d3ef42c8079fcf86659a05475b792fcee
darren.sargent <darren.sargent@cognizant.ai>
2021-10-19 11:02:29 -0700

    Improve name

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Index(): React.ReactElement {
         <meta name="description" content="Evolutionary AI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      LEAF v0.1.1
+      Unileaf v0.1.1
     </div>
   )
 }
