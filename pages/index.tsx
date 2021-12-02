692ebc464f50823189f3a60d9e7c9656d5c219f9
mohakamg <mohak.kant1amg@gmail.com>
2021-12-02 08:18:02 -0800

    Added assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import styled from "styled-components";
 
+import Image from 'next/image'
+
+
 const OuterContainer = styled.div`
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("/landingpagebackground.png");
     background-size: cover;
@@ -22,23 +25,80 @@ const Navbar = styled.div`
     align-items: center;
 `
 
-const NavbarLogo = styled.h4`
+const NavbarLogo = styled.h1`
     color: white;
+    font-size: 1.25rem;
 `
 
 const NavbarMiddleSection = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 12.5%;
+    grid-gap: 27.32%;
+    width: 28.59%;
 `
 
 const NavbarItem = styled.a`
     text-decoration: none;
     color: white;
-    font-size: 1rem;
+    font-size: .938rem;
     padding: 0;
     margin: 0;
     text-align: center;
+`
+
+const LeftRight = styled.div`
+    display: grid;
+    grid-template-columns: 4fr 1fr;
+    // grid-gap: 18.125rem;
+`
+
+const HeaderLineOne = styled.h1`
+    width: auto; height: auto;
+    margin: 0; padding: 0;
+    margin-top: 8.438rem;
+    color: white;
+    font-size: 5rem;
+`
+const HeaderLineTwo = styled.h1`
+    width: auto; height: auto;
+    margin: 0; padding: 0;
+    margin-top: -2.225%;
+    color: white;
+    font-size: 5rem;
+`
+
+const GetStartedButton = styled.button`
+    margin-top: 4rem;
+    color: white;
+    font-size: 1.25rem;
+    padding-bottom: 0.313rem;
+    border-bottom: 2px solid #FDB716;
+`
+
+const Description = styled.p`
+    color: white;
+    font-size: 1.219rem;
+    width: 39rem;
+    margin-top: 5.021rem;
+`
+
+const Right = styled.div`
+    margin-top: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ImageDiv = styled.div`
+    width: 15.563rem; height: 29.375rem;
+`
+
+const D2DText = styled.p`
+    color: white;
+    font-size: 2.5rem;
+    width: 20.563rem;
+    margin-top: 5.021rem;
+    text-align: right;
 `
 
 export default function Index(): React.ReactElement {
@@ -54,6 +114,27 @@ export default function Index(): React.ReactElement {
               </NavbarMiddleSection>
               <NavbarItem>Partner</NavbarItem>
           </Navbar>
+          <LeftRight>
+              <div>
+                  <HeaderLineOne>OPTIMIZE YOUR</HeaderLineOne>
+                  <HeaderLineTwo>BUISNESS</HeaderLineTwo>
+                  <GetStartedButton>Get Started</GetStartedButton>
+                  <Description>
+                      Research and builds applications with modern AI techniques such as evolutionary computation and deep learning across disciplines using LEAF (Learning and Evolutionary AI Framework)
+                  </Description>
+              </div>
+              <Right>
+                  <ImageDiv>
+                      <Image
+                          src="/nnleaf.svg"
+                          width="249"
+                          height="587"
+                          layout="responsive"
+                      />
+                  </ImageDiv>
+                  <D2DText>Data to Decisions</D2DText>
+              </Right>
+          </LeftRight>
 
       </Marginer>
     </OuterContainer>
