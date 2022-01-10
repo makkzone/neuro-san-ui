054b7cc40eb4f10086f1bf9e870a2422b61fbec4
darren.sargent <darren.sargent@cognizant.ai>
2022-01-09 22:29:44 -0800

    UN-337 Add spinners for more user actions

--- utils/transformation.ts
+++ utils/transformation.ts
@@ -7,3 +7,13 @@ export default function getCheckedOnly(obj: Object) {
     return Object.fromEntries(
         Object.entries(obj).filter(([_, val]) => val));
 }
+
+// Removes the first instance of "value" from array"arr", if found, and returns the modified
+// array
+export function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
