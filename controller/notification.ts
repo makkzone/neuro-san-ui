7a944d79d7da3d9bb765eacf9b0828cc59fbf8be
dsargent <darren.sargent@cognizant.ai>
2022-03-31 13:02:35 -0700

    UN-466 UX improvements -- notifications, better indication when run is in progress

--- controller/notification.ts
+++ controller/notification.ts
@@ -1,9 +1,31 @@
-import { notification } from 'antd';
+import {notification} from 'antd';
+
+export enum NotificationType {
+    "success",
+    "info",
+    "warning",
+    "error"
+}
 
 export interface NotificationProps {
     readonly Type: string,
     readonly Message: string,
     readonly Description: string
+}
+
+/**
+ * Convenience method to allow sending notifications to user with a one-liner.
+ * Simply wraps @Notification function
+ * @param nt Indicates whether it's error, warning, info etc.
+ * @param message Brief summary of the notification
+ * @param description More complete description of the notification
+ */
+export function sendNotification(nt: NotificationType, message: string, description: string): void {
+    Notification({
+        Type: NotificationType[nt],
+        Message: message,
+        Description: description
+    })
 }
 
 export default function Notification(props: NotificationProps) {
