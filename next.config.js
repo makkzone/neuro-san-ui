38d2b6d5463c086bac5a0c22e79707dbbcc9cb40
dsargent <darren.sargent@cognizant.ai>
2022-04-11 22:10:26 -0700

    UN-224 Improvements to eslint
    1) Add in directories that were being missed
    2) Add more linting rules (which increased the current warning and error load)
    3) Minor improvements to linting script

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  publicRuntimeConfig: {
-    // if the md_server_url is not set it defaults to invalid url
-    // this way we don't accidently point prod->staging or vice versa
-    md_server_url: process.env.MD_SERVER_URL ?? "MD_SERVER_URL_must_be_set",
-  },
-  entry: path.resolve(__dirname, 'main.js'),
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+    eslint: {
+        ignoreDuringBuilds: false,
+        dirs: ['components', 'controller', 'pages', 'public', 'styles', 'utils', '.']
+    },
+
+    publicRuntimeConfig: {
+        // if the md_server_url is not set it defaults to invalid url
+        // this way we don't accidently point prod->staging or vice versa
+        md_server_url: process.env.MD_SERVER_URL ?? "MD_SERVER_URL_must_be_set",
+    },
+    entry: path.resolve(__dirname, 'main.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.min.js',
