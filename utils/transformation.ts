d353771a6ddeacc4e66cbe554e09658e0f780749
dsargent <darren.sargent@cognizant.ai>
2022-04-14 12:57:07 -0700

    UN-481 Add first unit test for UI 🎉
    This brings in the "jest" framework for js and ts testing

--- utils/transformation.ts
+++ utils/transformation.ts
@@ -3,7 +3,7 @@
 // Removes the first instance of "value" from array"arr", if found, and returns the modified
 // array
 export function removeItemOnce(arr, value) {
-    var index = arr.indexOf(value);
+    const index = arr.indexOf(value);
     if (index > -1) {
         arr.splice(index, 1);
     }
