ccefa6319232b64acd28fcddc3a1e515770e23fb
dsargent <darren.sargent@cognizant.ai>
2022-05-19 13:20:02 -0700

    UN-550 Better error handling in DMS
    Also clean up notifications utility -- remove unnecessary middleman.

--- controller/notification.ts
+++ controller/notification.ts
@@ -7,11 +7,11 @@ export enum NotificationType {
     "error"
 }
 
-export interface NotificationProps {
-    readonly Type: string,
-    readonly Message: string,
-    readonly Description: string
-}
+// Display warning and error notification popups for this many seconds
+const ERROR_WARNING_NOTIFICATION_DURATION_SECS = 15;
+
+// Display info notification popups for this many seconds
+const SUCCESS_NOTIFICATION_DURATION_SECS = 5;
 
 /**
  * Convenience method to allow sending notifications to user with a one-liner.
@@ -21,22 +21,27 @@ export interface NotificationProps {
  * @param description More complete description of the notification
  */
 export function sendNotification(nt: NotificationType, message: string, description = ""): void {
-    Notification({
-        Type: NotificationType[nt],
-        Message: message,
-        Description: description
+    // Log a copy to the console for troubleshooting
+    console.debug(`Notification: Message: "${message}" Description: "${description}"`)
+
+    // Show error and warnings for longer
+    let duration: number
+    switch (nt) {
+        case NotificationType.info:
+        case NotificationType.success:
+            duration = SUCCESS_NOTIFICATION_DURATION_SECS
+            break
+        case NotificationType.warning:
+        case NotificationType.error:
+        default:
+            duration = ERROR_WARNING_NOTIFICATION_DURATION_SECS
+            break
+    }
+
+    // Send the notification with antd
+    notification[NotificationType[nt]]({
+        message: message,
+        description: description,
+        duration: duration
     })
 }
-
-export default function Notification(props: NotificationProps) {
-
-    /*
-    This function extends the AndD notification to be a repeatable,
-    easy to use component
-    */
-    notification[props.Type]({
-      message: props.Message,
-      description:
-        props.Description
-    });
-  };
