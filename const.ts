03106674cb20c2bf9f0b0fd3e59896a053da82f2
Daniel Lewandowski <daniel.lewandowski@cognizant.com>
2023-05-24 11:39:41 -0400

    name update on inner pages

--- const.ts
+++ const.ts
@@ -12,7 +12,7 @@ export const MD_BASE_URL: string = publicRuntimeConfig.md_server_url
 export const ENABLE_AUTHENTICATION: boolean = Boolean(JSON.parse(publicRuntimeConfig.enableAuthentication))
 
 // Name to use for application
-export const LOGO: string = "Cognizant Neuro® AI"
+export const LOGO: string = "Cognizant Neuro® AI Model Orchestrator"
 
 // Build version (passed in from build system)
 export const UNILEAF_VERSION: string = publicRuntimeConfig.unileafVersion
