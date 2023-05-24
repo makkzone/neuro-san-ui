2edbb07d72df1b1a97358da25d2e6cdb9a5326e9
Daniel Lewandowski <daniel.lewandowski@cognizant.com>
2023-05-24 12:15:57 -0400

    headline eyebrow fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -111,7 +111,7 @@ export default function Index(): React.ReactElement {
           </Navbar>
           <LeftRight id="main">
               <div id="main-div">
-                  <HeaderLineOne id="optimize-your"><small>Cognizant Neuro® AI Model Orchestrator</small>Accelerate generative AI solutions at enterprise scale</HeaderLineOne>
+                  <HeaderLineOne id="optimize-your"><small id="headline-eyebrow" class="d-block h5 text-white">Cognizant Neuro® AI Model Orchestrator</small>Accelerate generative AI solutions at enterprise scale</HeaderLineOne>
                   {/* <HeaderLineTwo id="business">BUSINESS</HeaderLineTwo> */}
                   <Link id="get-started-link" href={`/projects`} >
                       <GetStartedButton id="get-started">Get started</GetStartedButton>
