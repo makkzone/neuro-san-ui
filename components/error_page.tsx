eff2f155723dd54a57428b22386a34e74cf8fb8e
Darren Sargent <darren.sargent@cognizant.com>
2023-08-16 17:29:10 -0700

    UN-1325 Make error boundary page a little friendlier

--- components/error_page.tsx
+++ components/error_page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {GENERIC_LOGO, LOGO} from "../const";
 import Navbar from "./navbar";
 import {useRouter} from "next/router";
+import BlankLines from "./blanklines";
 
 /**
  * This is the page that will be shown to users when the outer error boundary is triggered
@@ -15,12 +16,28 @@ export default function ErrorPage({id, errorText}): React.ReactElement {
     const isGeneric = "generic" in router.query
 
     return <div id={id} className="container">
-    <Navbar id="navbar-id" Logo={isGeneric ? GENERIC_LOGO : LOGO} WithBreadcrumbs={true}/>
-        <div id="error-div" style={{fontSize: "xx-large", color: "red", marginBottom: 16}}>
-            An internal error occurred. Please report the following error to the LEAF team:
+        <Navbar id="navbar-id" Logo={isGeneric ? GENERIC_LOGO : LOGO} WithBreadcrumbs={true}/>
+        <h4 id="error-header" style={{color: "red"}}>
+            Oops, an internal error occurred on our end.
+        </h4>
+        <div id="error-div-1"><b id="bold-1">There&apos;s no need to worry – you didn&apos;t do anything
+            wrong.</b> This is a bug in our server and we&apos;ll fix it as soon as possible.
+            <BlankLines id="bl1" numLines={2} />
+            To attempt to recover from the error, try these steps:
+            <BlankLines id="bl2" numLines={2} />
+            <ul id="error-boundary-advice-list" className="list-decimal list-inside">
+                <li id="error-advice-refresh">Refresh the page to see if the error goes away.</li>
+                <li id="error-advice-reload">Close and re-open your browser, then navigate
+                    back to the page where you encountered the error.</li>
+            </ul>
+            <BlankLines id="bl3" numLines={1} />
+            If none of that helps, please report the following error to the LEAF team:
         </div>
-        <div id="error-text-div" style={{fontFamily: "monospace", marginBottom: 16}}>{errorText}</div>
-        More information may be available in the browser console.
+        <BlankLines  id="bl4" numLines={1} />
+        <div id="error-text-div" style={{fontFamily: "monospace"}}>{errorText}</div>
+        <BlankLines id="bl5" numLines={1} />
+        More information may be available in the browser console. Please include such information in your report
+        if possible.
     </div>
 }
 
