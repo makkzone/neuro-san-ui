d78528ca5d6953a08f4f527ba65f27760e6a6035
Michael Cheung <michael.cheung@cognizant.com>
2024-03-21 15:33:23 -0700

    Revert "populate uncertainty node params if its empty"
    This reverts commit 24e45db613f1f03c9331b6336eecbd34543f3ad1.

--- utils/transformation.ts
+++ utils/transformation.ts
@@ -30,15 +30,11 @@ export const consolidateFlow = (flowNodes) => {
             node?.type === "llmnode"
 
         const isUncertaintyNode = node?.type === "uncertaintymodelnode"
-        const uncertaintymodelnodeNoParams =
-            isUncertaintyNode &&
-            (!copyNode?.data?.ParentNodeState?.params ||
-                Object.keys(copyNode?.data?.ParentNodeState?.params).length < 1)
 
         if (isLLMNode && !node?.data?.ParentNodeState?.params) {
             copyNode.data.ParentNodeState = {params: node.data.ParentNodeState}
-        } else if (uncertaintymodelnodeNoParams) {
-            copyNode.data.ParentNodeState = {params: node.data.ParentUncertaintyNodeState}
+        } else if (isUncertaintyNode && node?.data?.ParameterSet) {
+            copyNode.data.ParentNodeState = {params: node.data.ParameterSet}
         } else if (!node?.data?.ParentNodeState && node?.data?.ParentPredictorState) {
             copyNode.data.ParentNodeState = copyNode.data.ParentPredictorState
             if (node?.data?.ParentPredictorState?.predictorParams) {
