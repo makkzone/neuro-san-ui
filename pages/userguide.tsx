76cb599f5985ba0bc28c426cedd84002fb354fd7
Darren Sargent <darren.sargent@cognizant.com>
2024-06-06 21:14:38 -0700

    UN-2152 Fix TOC links in user guide

--- pages/userguide.tsx
+++ pages/userguide.tsx
@@ -1,6 +1,11 @@
 import {useEffect, useState} from "react"
 import ReactMarkdown from "react-markdown"
 import rehypeRaw from "rehype-raw"
+import rehypeSlug from "rehype-slug"
+import remarkToc from "remark-toc"
+
+// Path to user guide Markdown doc on the server
+const USER_GUIDE_PATH = "user_guide.md"
 
 // Main function.
 // Has to be export default for NextJS so tell ts-prune to ignore
@@ -8,25 +13,39 @@ import rehypeRaw from "rehype-raw"
 export default function UserGuide() {
     const [userGuide, setUserGuide] = useState(null)
 
-    const getData = async () => {
-        fetch("user_guide.md", {
-            headers: {
-                "Content-Type": "text/markdown",
-                Accept: "text/markdown",
-            },
-        })
-            .then(async (response) => response.text())
-            .then((text) => setUserGuide(text))
+    async function getData() {
+        try {
+            // fetch guide from server
+            const result = await fetch(USER_GUIDE_PATH, {
+                headers: {
+                    "Content-Type": "text/markdown",
+                    Accept: "text/markdown",
+                },
+            })
+
+            // Check if the request was successful
+            if (!result.ok) {
+                setUserGuide(`## Unable to load user guide. Internal error: http ${result.status} ${result.statusText}`)
+                return
+            }
+
+            // Set user guide content
+            setUserGuide(await result.text())
+        } catch (e) {
+            setUserGuide(`## Unable to load user guide. Internal error: ${e}`)
+        }
     }
+
     useEffect(() => {
-        getData()
+        void getData()
     }, [])
 
     return (
         <>
             {/* 2/6/23 DEF - ReactMarkdown does not have an id property when compiling */}
             <ReactMarkdown // eslint-disable-line enforce-ids-in-jsx/missing-ids
-                rehypePlugins={[rehypeRaw]}
+                rehypePlugins={[rehypeRaw, rehypeSlug]}
+                remarkPlugins={[[remarkToc, {heading: "Table of Contents", tight: true}]]}
                 className="prose"
             >
                 {userGuide}
