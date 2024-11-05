e435917dcce286708fd10563de7189bf34bc14c7
swensel <seanwensel@gmail.com>
2024-11-04 20:31:29 -0800

    Dall-e.

--- components/internal/uiMockupGenerator/UIMockupGenerator.tsx
+++ components/internal/uiMockupGenerator/UIMockupGenerator.tsx
@@ -1,9 +1,5 @@
-import NextImage from "next/image"
-import {StringToStringOrNumber} from "../../../controller/base_types"
-import {Run} from "../../../controller/run/types"
 import {useRef, useState} from "react"
 import {sendDalleQuery} from "../../../controller/dall-e/dall-e"
-import {HumanMessage} from "@langchain/core/messages"
 import NewBar from "../../newbar"
 import {Button, Container} from "react-bootstrap"
 import {MaximumBlue} from "../../../const"
@@ -15,43 +11,24 @@ interface UIMockupGeneratorProps {
 // #endregion: Types
 
 export const UIMockupGenerator: React.FC<UIMockupGeneratorProps> = ({userQuery}) => {
-    const [mockupURL, setMockupURL] = useState<string>("") // not sure which type yet, maybe a blob?
+    const [mockupURL, setMockupURL] = useState<string>("")
     const [showUIMockup, setShowUIMockup] = useState<boolean>(false)
 
     // Controller for cancelling fetch request
     const controller = useRef<AbortController>(null)
 
-    const generateMockup = () => {
-        // eslint-disable-next-line max-len
-        setMockupURL(
-            "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEirznXLjClkxMHiW19yZlpe-1qL27ilMi5e57nsML60zLGVql2ZVIv0qDDEN32mWcTJSZcOBsP_ga5k1vt3o7gPrQD1DTh2pUUWmCeYyxs2JuFR0F3_ne7YpseQRWfy6coMNoggJa_zNQY/w1200-h630-p-k-no-nu/Dilbert%2520Software%2520Demo%5B1%5D.jpg"
-        )
-        setShowUIMockup(true)
-        sendQueryToDalle()
-    }
-
     // Sends query to backend to send to Dall-e via Langchain.
-    async function sendQueryToDalle() {
+    async function sendUserQueryToDalle() {
         try {
             const abortController = new AbortController()
             controller.current = abortController
 
-            // Send the query to the server. Response will be streamed to our callback which updates the output
-            // display as tokens are received.
-            /*await sendDalleQuery(
-                userQuery,
-                contextInputs,
-                prescriptorUrl,
-                predictorUrls,
-                renderMockup,
-                abortController.signal,
-                run,
-                [new HumanMessage(userQuery)],
-                projectName,
-                projectDescription
-            )*/
+            const response = await sendDalleQuery(userQuery)
+            setMockupURL(response.imageURL)
+            setShowUIMockup(true) // TODO: Switch to magic wand instead of big section
         } catch (error) {
             // log error to console
+            // TODO: Check that error handling is working
             console.error(error)
         }
     }
@@ -68,7 +45,7 @@ export const UIMockupGenerator: React.FC<UIMockupGeneratorProps> = ({userQuery})
                 <Button
                     className="mt-4 mb-4"
                     id="dms-mockup"
-                    onClick={generateMockup}
+                    onClick={sendUserQueryToDalle}
                     size="lg"
                     style={{background: MaximumBlue, borderColor: MaximumBlue, width: "100%"}}
                 >
--- controller/dall-e/dall-e.ts
+++ controller/dall-e/dall-e.ts
@@ -1,50 +1,22 @@
 /**
- * Controller module for generating Dall-e UI mokcups
- */
-import {BaseMessage} from "@langchain/core/messages"
-
-import {sendLlmRequest} from "../llm/llm_chat"
-import {Run} from "../run/types"
-
-/**
  * Access the API for DMS chat. Client is responsible for catching and handling exceptions that may be thrown.
  *
  * @param userQuery The user's query.
- * @param context The context part of the CAO.
- * @param prescriptorUrl The URL of the prescriptor for inference.
- * @param predictorUrls The URLs of the predictors for inference.
- * @param callback The callback function to be called when a chunk of data is received from the server.
- * @param signal The AbortSignal object to be used for aborting the request.
- * @param run The current Run for the DMS page
- * @param chatHistory The chat history to be sent to the server. Contains user requests and server responses.
- * @param projectName The name of the project the user is working on.
- * @param projectDescription The description of the project the user is working on.
  */
-export async function sendDalleQuery(
-    userQuery: string,
-    context: object,
-    prescriptorUrl: string,
-    predictorUrls: string[],
-    callback: (string) => void,
-    signal: AbortSignal,
-    run: Run,
-    chatHistory?: BaseMessage[],
-    projectName?: string,
-    projectDescription?: string
-) {
-    await sendLlmRequest(
-        callback,
-        signal,
-        "/api/gpt/dall-e",
-        {
-            prescriptorUrl,
-            predictorUrls,
-            context,
-            run,
-            projectName,
-            projectDescription,
+export async function sendDalleQuery(userQuery: string) {
+    const res = await fetch("/api/gpt/dall-e", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
         },
-        userQuery,
-        chatHistory
-    )
+        body: userQuery,
+    })
+
+    // Check if the request was successful
+    if (!res.ok) {
+        throw new Error(`Failed to fetch: ${res.statusText} error code ${res.status}`)
+    }
+
+    return res.json()
 }
