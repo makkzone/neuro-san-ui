774fa2bdd06217996eb0af05a28e6519c2294cd9
Darren Sargent <darren.sargent@cognizant.com>
2025-01-16 14:54:29 -0800

    UN-2782 Adjust coverage thresholds

--- jest_component.config.ts
+++ jest_component.config.ts
@@ -20,10 +20,10 @@ const config: Config.InitialOptions = {
     // For details on these settings: https://jestjs.io/docs/configuration
     coverageThreshold: {
         global: {
-            statements: 26.43,
+            statements: 26.41,
             branches: 24.7,
             functions: 25.75,
-            lines: 26.1,
+            lines: 26.09,
         },
     },
 }
