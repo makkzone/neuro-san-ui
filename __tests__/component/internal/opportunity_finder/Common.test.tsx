542e12ccded4fe9d8bb803f74c9cf47c6890709b
Darren Sargent <darren.sargent@cognizant.com>
2025-01-30 11:53:32 -0800

    UN-2761 Add test for "common" module

--- __tests__/component/internal/opportunity_finder/Common.test.tsx
+++ __tests__/component/internal/opportunity_finder/Common.test.tsx
@@ -1,1 +1,19 @@
-/* TBD */
+/**
+ * Tests for Opp Finder common logic
+ */
+
+import {experimentGeneratedMessage} from "../../../../components/internal/opportunity_finder/common"
+
+describe("Common component tests", () => {
+    it("Should generate a valid experiment complete item", () => {
+        const projectUrl = new URL("https://example.com")
+        const experimentGeneratedItem = experimentGeneratedMessage(projectUrl)
+
+        // make sure it contains a link
+        expect(experimentGeneratedItem.props.children).toContainEqual(expect.objectContaining({type: "a"}))
+
+        // retrieve the link
+        const link = experimentGeneratedItem.props.children.find((child) => child.type === "a")
+        expect(link.props.href).toContain(projectUrl.toString())
+    })
+})
