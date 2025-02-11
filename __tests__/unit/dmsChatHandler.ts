d4eb4989d8711fa78db1c2879907287a54a8992e
Darren Sargent <darren.sargent@cognizant.com>
2025-02-11 11:56:28 -0800

    UN-619 Use new env var for mesh-internal service calls

---
+++ __tests__/unit/dmsChatHandler.ts
@@ -0,0 +1,56 @@
+/* Unit tests for the DMS Chat NodeJS module */
+
+import httpStatus from "http-status"
+import {NextApiRequest, NextApiResponse} from "next"
+import "openai/shims/node"
+import {createMocks, RequestOptions} from "node-mocks-http"
+
+import handler from "../../pages/api/gpt/dmschat"
+
+describe("handler", () => {
+    let request: RequestOptions
+
+    beforeEach(() => {
+        request = {
+            method: "POST",
+            headers: {
+                Accept: "text/plain",
+                "Content-Type": "application/json",
+            },
+            body: {
+                userQuery: "This is a user query",
+            },
+        }
+    })
+
+    it("Should reject for missing user query", () => {
+        delete request.body.userQuery
+        const {req, res} = createMocks<NextApiRequest, NextApiResponse>(request)
+        handler(req, res)
+        expect(res.statusCode).toBe(httpStatus.BAD_REQUEST)
+        expect(res._getJSONData().error).toBe("Bad request")
+    })
+
+    it("Should reject for wrong method", () => {
+        request.method = "GET" // intentionally wrong, should be POST
+        const {req, res} = createMocks<NextApiRequest, NextApiResponse>(request)
+        handler(req, res)
+        expect(res.statusCode).toBe(httpStatus.METHOD_NOT_ALLOWED)
+        expect(res._getJSONData().error).toBe("Method not allowed")
+    })
+
+    it("Should reject for missing OpenAI API key", () => {
+        const {req, res} = createMocks<NextApiRequest, NextApiResponse>(request)
+        handler(req, res)
+        expect(res.statusCode).toBe(httpStatus.INTERNAL_SERVER_ERROR)
+        expect(res._getJSONData().error).toBe("Could not find API Key")
+    })
+
+    it("Should reject for missing server API URL", () => {
+        process.env.OPENAI_API_KEY = "test"
+        const {req, res} = createMocks<NextApiRequest, NextApiResponse>(request)
+        handler(req, res)
+        expect(res.statusCode).toBe(httpStatus.INTERNAL_SERVER_ERROR)
+        expect(res._getJSONData().error).toBe("Could not find MD_SERVER_URL or INTERNAL_GATEWAY_URL")
+    })
+})
