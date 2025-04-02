4df99a35f87e63c231a4c7161cbb3a3d5515f77d
Darren Sargent <darren.sargent@cognizant.com>
2025-04-02 12:49:57 -0700

    UN-1275 Add transition effect to open/close

--- components/ChatBot/ChatBot.tsx
+++ components/ChatBot/ChatBot.tsx
@@ -1,5 +1,6 @@
 import ContactSupportIcon from "@mui/icons-material/ContactSupport"
 import Box from "@mui/material/Box"
+import Grow from "@mui/material/Grow"
 import {FC, useState} from "react"
 
 import {CHATBOT_ENDPOINT} from "../../controller/llm/endpoints"
@@ -17,56 +18,66 @@ export const ChatBot: FC<ChatBotProps> = ({id, userAvatar, pageContext}) => {
     const [chatOpen, setChatOpen] = useState<boolean>(false)
     const [isAwaitingLlm, setIsAwaitingLlm] = useState<boolean>(false)
 
-    return chatOpen ? (
-        <Box
-            id={id}
-            sx={{
-                position: "fixed",
-                bottom: 50,
-                right: "2rem",
-                height: "60%",
-                maxWidth: 400,
-                background: "var(--bs-white)",
-                boxShadow: "0 0px 2px 0 rgba(0, 0, 0, 0.15)",
-                borderRadius: "var(--bs-border-radius)",
-                borderWidth: 1,
-                borderColor: "var(--bs-gray-light)",
-                zIndex: ZIndexLayers.LAYER_2,
-            }}
-        >
-            <ChatCommon
-                id="chatbot-window"
-                currentUser="test"
-                setIsAwaitingLlm={setIsAwaitingLlm}
-                isAwaitingLlm={isAwaitingLlm}
-                targetAgent={LegacyAgentType.ChatBot}
-                userImage={userAvatar}
-                legacyAgentEndpoint={CHATBOT_ENDPOINT}
-                extraParams={{pageContext}}
-                backgroundColor="var(--bs-tertiary-blue)"
-                title="Cognizant Neuro AI Assistant"
-                onClose={() => setChatOpen(false)}
-            />
-        </Box>
-    ) : (
-        <Box
-            sx={{
-                display: "flex",
-                alignItems: "center",
-                backgroundColor: "var(--bs-white)",
-                borderRadius: "50%",
-                bottom: 16,
-                boxShadow: 4,
-                cursor: "pointer",
-                height: 40,
-                justifyContent: "center",
-                maxWidth: 40,
-                padding: "1rem",
-                position: "fixed",
-                right: 16,
-            }}
-        >
-            <ContactSupportIcon onClick={() => setChatOpen(true)} />
-        </Box>
+    return (
+        <>
+            <Grow
+                in={chatOpen}
+                timeout={300}
+                style={{transformOrigin: "bottom right"}}
+                easing="ease-in-out"
+            >
+                <Box
+                    id={id}
+                    sx={{
+                        position: "fixed",
+                        bottom: 50,
+                        right: "2rem",
+                        height: "60%",
+                        maxWidth: 400,
+                        background: "var(--bs-white)",
+                        boxShadow: "0 0px 2px 0 rgba(0, 0, 0, 0.15)",
+                        borderRadius: "var(--bs-border-radius)",
+                        borderWidth: 1,
+                        borderColor: "var(--bs-gray-light)",
+                        zIndex: ZIndexLayers.LAYER_2,
+                    }}
+                >
+                    <ChatCommon
+                        id="chatbot-window"
+                        currentUser="test"
+                        setIsAwaitingLlm={setIsAwaitingLlm}
+                        isAwaitingLlm={isAwaitingLlm}
+                        targetAgent={LegacyAgentType.ChatBot}
+                        userImage={userAvatar}
+                        legacyAgentEndpoint={CHATBOT_ENDPOINT}
+                        extraParams={{pageContext}}
+                        backgroundColor="var(--bs-tertiary-blue)"
+                        title="Cognizant Neuro AI Assistant"
+                        onClose={() => setChatOpen(false)}
+                    />
+                </Box>
+            </Grow>
+            {!chatOpen && (
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        backgroundColor: "var(--bs-white)",
+                        borderRadius: "50%",
+                        bottom: 16,
+                        boxShadow: 4,
+                        cursor: "pointer",
+                        height: 40,
+                        justifyContent: "center",
+                        maxWidth: 40,
+                        padding: "1rem",
+                        position: "fixed",
+                        right: 16,
+                    }}
+                >
+                    <ContactSupportIcon onClick={() => setChatOpen(true)} />
+                </Box>
+            )}
+        </>
     )
 }
