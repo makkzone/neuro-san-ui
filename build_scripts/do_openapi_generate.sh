7d285590e8c6785aa2bc48ebdb0f7265f9cdff84
Darren Sargent <darren.sargent@cognizant.com>
2025-04-28 18:38:11 -0700

    UN-3080 Add build logic to generate OpenAPI Typescript stubs

--- build_scripts/do_openapi_generate.sh
+++ build_scripts/do_openapi_generate.sh
@@ -1,12 +1,13 @@
-#yarn swagger-typescript-api generate \
-#  --module-name-index 1 \
-#  --unwrap-response-data -t \
-#  ./client/templates \
-#  --path swagger.json \
-#  --output ./generated/rest/neuro-san \
-#  --name NeuroSanClient.ts \
-#  --silent
+#!/usr/bin/env bash
 
+# This script generates types for the web-based Neuro-san API based on an OpenAPI spec.
+#
+# See https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html for what these do
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 
+export NEURO_SAN_SERVER_URL="https://neuro-san.decisionai.ml"
 
-yarn openapi-typescript https://neuro-san.decisionai.ml/api/v1/docs -o ./generated/neuro-san/NeuroSanClient.d.ts
+yarn openapi-typescript "${NEURO_SAN_SERVER_URL}/api/v1/docs" -o ./generated/neuro-san/NeuroSanClient.d.ts
