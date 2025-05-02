f19f04819473ea1e75f2d19ef5e17b9d25de82bc
Sean Wensel <seanwensel@gmail.com>
2025-05-01 21:27:01 -0700

    Add NeuroSanIndirect

--- jest_unit.config.ts
+++ jest_unit.config.ts
@@ -12,10 +12,10 @@ const config: Config.InitialOptions = {
     // For details on these settings: https://jestjs.io/docs/configuration
     coverageThreshold: {
         global: {
-            statements: 53.66,
-            branches: 44.71,
-            functions: 48.09,
-            lines: 53.42,
+            statements: 53.34,
+            branches: 44.2,
+            functions: 47.87,
+            lines: 53.08,
         },
     },
 }
